cd18d79b9763245d199e16cfe56e333a87587491
Minke Zhang <minke.zhang@gmail.com>
2022-12-16 22:51:12 -0800

    Add agent heading

--- agent/agent.go
+++ agent/agent.go
@@ -3,6 +3,7 @@ package agent
 import (
 	"github.com/downflux/game-db/agent/mask"
 	"github.com/downflux/go-geometry/2d/vector"
+	"github.com/downflux/go-geometry/2d/vector/polar"
 )
 
 type A struct {
@@ -10,12 +11,16 @@ type A struct {
 	velocity vector.M
 	radius   float64
 
-	// theta is a radian angle relative to the positive X-axis.
-	theta float64
+	// heading is a unit polar vector whose angular component is oriented to
+	// the positive X-axis. The angle is calculated according to normal 2D
+	// rotational rules, i.e. a vector lying on the positive Y-axis has an
+	// angular componet of π / 2.
+	heading polar.M
 
-	maxVelocity        float64
-	maxAngularVelocity float64
-	maxAcceleration    float64
+	maxVelocity            float64
+	maxAngularVelocity     float64
+	maxAcceleration        float64
+	maxAngularAcceleration float64
 
 	mask mask.M
 }
@@ -23,7 +28,7 @@ type A struct {
 func (a *A) Position() vector.V { return a.position.V() }
 func (a *A) Velocity() vector.V { return a.velocity.V() }
 func (a *A) Radius() float64    { return a.radius }
-func (a *A) Theta() float64     { return a.theta }
+func (a *A) Heading() polar.V   { return a.heading.V() }
 
 // IgnoreCollision checks if two agents need to do any additional processing in
 // the case that their bounding circles overlap.
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/downflux/game-db
 
 go 1.19
 
-require github.com/downflux/go-geometry v0.15.0
+require github.com/downflux/go-geometry v0.15.1
