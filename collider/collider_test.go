73248ba2422957be4c5ecea64d857e9b69b0c427
Minke Zhang <minke.zhang@gmail.com>
2023-01-04 02:26:30 -0800

    Update collider

--- collider/collider_test.go
+++ collider/collider_test.go
@@ -8,15 +8,12 @@ import (
 	"time"
 
 	"github.com/downflux/go-bvh/id"
-	"github.com/downflux/go-collider/agent"
-	"github.com/downflux/go-collider/feature"
-	"github.com/downflux/go-collider/internal/collider"
-	"github.com/downflux/go-collider/mask"
+	"github.com/downflux/go-database/agent"
+	"github.com/downflux/go-database/database"
+	"github.com/downflux/go-database/feature"
+	"github.com/downflux/go-database/flags"
 	"github.com/downflux/go-geometry/2d/vector"
 	"github.com/downflux/go-geometry/2d/vector/polar"
-	"github.com/downflux/go-geometry/nd/hyperrectangle"
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 )
 
 const (
@@ -31,246 +28,63 @@ func rv(min, max float64) vector.V {
 	}
 }
 
-func TestQueryFeatures(t *testing.T) {
-	type config struct {
-		name     string
-		collider *C
-		x        id.ID
-		q        hyperrectangle.R
-		want     []id.ID
-	}
-
-	configs := []config{
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{5, 15},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   5,
-				Mask:     mask.MSizeSmall,
-			})
-			f := collider.InsertFeature(feature.O{
-				Min: vector.V{10, 10},
-				Max: vector.V{20, 20},
-			})
-			return config{
-				name:     "Touch",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{f.ID()},
-			}
-		}(),
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{9, 10},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
-			})
-			f := collider.InsertFeature(feature.O{
-				Min: vector.V{10, 10},
-				Max: vector.V{20, 20},
-			})
-			return config{
-				name:     "Slide",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{f.ID()},
-			}
-		}(),
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{5, 5},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   5,
-				Mask:     mask.MSizeSmall,
-			})
-			f := collider.InsertFeature(feature.O{
-				Min: vector.V{10, 10},
-				Max: vector.V{20, 20},
-			})
-			return config{
-				name:     "Corner",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{f.ID()},
-			}
-		}(),
-	}
-
-	for _, c := range configs {
-		t.Run(c.name, func(t *testing.T) {
-			got := c.collider.QueryFeatures(c.q, func(f *feature.F) bool {
-				return collider.IsCollidingFeature(c.collider.getOrDie(c.x), f)
-			})
-			if diff := cmp.Diff(c.want, got, cmpopts.SortSlices(func(a, b id.ID) bool { return a < b })); diff != "" {
-				t.Errorf("QueryFeatures() mismatch (-want +got):\n%v", diff)
-			}
-		})
-	}
-}
-
-func TestQuery(t *testing.T) {
-	type config struct {
-		name     string
-		collider *C
-		x        id.ID
-		q        hyperrectangle.R
-		want     []id.ID
-	}
-
-	configs := []config{
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{10, 10},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
-			})
-			return config{
-				name:     "Exclude/Self",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{},
-			}
-		}(),
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{10, 10},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
-			})
-
-			b := collider.Insert(agent.O{
-				Position: a.Position(),
-				Velocity: a.Velocity(),
-				Heading:  a.Heading(),
-				Radius:   a.Radius(),
-				Mask:     mask.MSizeSmall,
-			})
-			collider.Insert(agent.O{
-				Position: a.Position(),
-				Velocity: a.Velocity(),
-				Heading:  a.Heading(),
-				Radius:   a.Radius(),
-				Mask:     mask.MSizeProjectile,
-			})
-			return config{
-				name:     "Exclude/Projectiles",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{b.ID()},
-			}
-		}(),
-		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{10, 10},
-				Velocity: vector.V{0, 0},
-				Heading:  polar.V{1, 0},
-				Radius:   1,
-				Mask:     mask.MSizeMedium,
-			})
-			b := collider.Insert(agent.O{
-				Position: a.Position(),
-				Velocity: a.Velocity(),
-				Heading:  a.Heading(),
-				Radius:   a.Radius(),
-				Mask:     mask.MSizeLarge,
-			})
-			collider.Insert(agent.O{
-				Position: a.Position(),
-				Velocity: a.Velocity(),
-				Heading:  a.Heading(),
-				Radius:   a.Radius(),
-				Mask:     mask.MSizeSmall,
-			})
-			return config{
-				name:     "Exclude/Squishable",
-				collider: collider,
-				x:        a.ID(),
-				q:        agent.AABB(a.Position(), a.Radius()),
-				want:     []id.ID{b.ID()},
-			}
-		}(),
-	}
-
-	for _, c := range configs {
-		t.Run(c.name, func(t *testing.T) {
-			got := c.collider.Query(c.q, func(b *agent.A) bool {
-				return collider.IsSquishableColliding(c.collider.getOrDie(c.x), b)
-			})
-			if diff := cmp.Diff(c.want, got, cmpopts.SortSlices(func(a, b id.ID) bool { return a < b })); diff != "" {
-				t.Errorf("query() mismatch (-want +got):\n%v", diff)
-			}
-		})
-	}
-}
-
 func TestTick(t *testing.T) {
 	type config struct {
 		name     string
 		collider *C
+		db       *database.DB
 		d        time.Duration
 		want     map[id.ID]vector.V
 	}
 
 	configs := []config{
 		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position: vector.V{0, 0},
-				Velocity: vector.V{1, 1},
-				Heading:  polar.V{1, math.Pi / 4},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
+			a := db.AgentInsert(agent.O{
+				Position:       vector.V{0, 0},
+				TargetVelocity: vector.V{1, 1},
+				Velocity:       vector.V{1, 1},
+				Heading:        polar.V{1, math.Pi / 4},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			b := collider.Insert(agent.O{
-				Position: vector.V{0, 1},
-				Velocity: vector.V{0, -1},
-				Heading:  polar.V{1, 3 * math.Pi / 2},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
+			b := db.AgentInsert(agent.O{
+				Position:       vector.V{0, 1},
+				TargetVelocity: vector.V{0, -1},
+				Velocity:       vector.V{0, -1},
+				Heading:        polar.V{1, 3 * math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			c := collider.Insert(agent.O{
-				Position: vector.V{0, -1},
-				Velocity: vector.V{0, 1},
-				Heading:  polar.V{1, math.Pi / 2},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
+			c := db.AgentInsert(agent.O{
+				Position:       vector.V{0, -1},
+				TargetVelocity: vector.V{0, 1},
+				Velocity:       vector.V{0, 1},
+				Heading:        polar.V{1, math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			d := collider.Insert(agent.O{
-				Position: vector.V{1, 0},
-				Velocity: vector.V{-1, 0},
-				Heading:  polar.V{1, math.Pi},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
+			d := db.AgentInsert(agent.O{
+				Position:       vector.V{1, 0},
+				TargetVelocity: vector.V{-1, 0},
+				Velocity:       vector.V{-1, 0},
+				Heading:        polar.V{1, math.Pi},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			e := collider.Insert(agent.O{
-				Position: vector.V{-1, 0},
-				Velocity: vector.V{1, 0},
-				Heading:  polar.V{1, 2 * math.Pi},
-				Radius:   1,
-				Mask:     mask.MSizeSmall,
+			e := db.AgentInsert(agent.O{
+				Position:       vector.V{-1, 0},
+				TargetVelocity: vector.V{1, 0},
+				Velocity:       vector.V{1, 0},
+				Heading:        polar.V{1, 2 * math.Pi},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
 			return config{
 				name:     "Stuck",
 				collider: collider,
+				db:       db,
 				d:        100 * time.Millisecond,
 				want: map[id.ID]vector.V{
 					a.ID(): vector.V{0, 0},
@@ -282,18 +96,21 @@ func TestTick(t *testing.T) {
 			}
 		}(),
 		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position:    vector.V{10, 10},
-				Velocity:    vector.V{1, 1},
-				MaxVelocity: math.Sqrt(2),
-				Heading:     polar.V{1, math.Pi / 4},
-				Radius:      1,
-				Mask:        mask.MSizeSmall,
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
+			a := db.AgentInsert(agent.O{
+				Position:       vector.V{10, 10},
+				TargetVelocity: vector.V{1, 1},
+				Velocity:       vector.V{1, 1},
+				MaxVelocity:    math.Sqrt(2),
+				Heading:        polar.V{1, math.Pi / 4},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
 			return config{
 				name:     "Trivial",
 				collider: collider,
+				db:       db,
 				d:        100 * time.Millisecond,
 				want: map[id.ID]vector.V{
 					a.ID(): vector.V{10.1, 10.1},
@@ -301,26 +118,30 @@ func TestTick(t *testing.T) {
 			}
 		}(),
 		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position:    vector.V{10, 10},
-				Velocity:    vector.V{0, 1},
-				MaxVelocity: 1,
-				Heading:     polar.V{1, math.Pi / 2},
-				Radius:      1,
-				Mask:        mask.MSizeSmall,
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
+			a := db.AgentInsert(agent.O{
+				Position:       vector.V{10, 10},
+				TargetVelocity: vector.V{0, 1},
+				Velocity:       vector.V{0, 1},
+				MaxVelocity:    1,
+				Heading:        polar.V{1, math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			b := collider.Insert(agent.O{
-				Position:    vector.V{10, 12},
-				Velocity:    vector.V{0, -1},
-				MaxVelocity: 1,
-				Heading:     polar.V{1, 3 * math.Pi / 2},
-				Radius:      1,
-				Mask:        mask.MSizeSmall,
+			b := db.AgentInsert(agent.O{
+				Position:       vector.V{10, 12},
+				TargetVelocity: vector.V{0, -1},
+				Velocity:       vector.V{0, -1},
+				MaxVelocity:    1,
+				Heading:        polar.V{1, 3 * math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
 			return config{
 				name:     "Collision",
 				collider: collider,
+				db:       db,
 				d:        100 * time.Millisecond,
 				want: map[id.ID]vector.V{
 					a.ID(): vector.V{10, 10},
@@ -329,26 +150,30 @@ func TestTick(t *testing.T) {
 			}
 		}(),
 		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
-				Position:    vector.V{10, 10},
-				Velocity:    vector.V{0, 1},
-				MaxVelocity: 1,
-				Heading:     polar.V{1, math.Pi / 2},
-				Radius:      1,
-				Mask:        mask.MSizeSmall,
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
+			a := db.AgentInsert(agent.O{
+				Position:       vector.V{10, 10},
+				TargetVelocity: vector.V{0, 1},
+				Velocity:       vector.V{0, 1},
+				MaxVelocity:    1,
+				Heading:        polar.V{1, math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeSmall,
 			})
-			b := collider.Insert(agent.O{
-				Position:    vector.V{10, 12},
-				Velocity:    vector.V{0, -1},
-				MaxVelocity: 1,
-				Heading:     polar.V{1, 3 * math.Pi / 2},
-				Radius:      1,
-				Mask:        mask.MSizeProjectile,
+			b := db.AgentInsert(agent.O{
+				Position:       vector.V{10, 12},
+				TargetVelocity: vector.V{0, -1},
+				Velocity:       vector.V{0, -1},
+				MaxVelocity:    1,
+				Heading:        polar.V{1, 3 * math.Pi / 2},
+				Radius:         1,
+				Flags:          flags.FSizeProjectile,
 			})
 			return config{
 				name:     "Collision/IgnoreProjectile",
 				collider: collider,
+				db:       db,
 				d:        100 * time.Millisecond,
 				want: map[id.ID]vector.V{
 					a.ID(): vector.V{10, 10.1},
@@ -357,18 +182,20 @@ func TestTick(t *testing.T) {
 			}
 		}(),
 		func() config {
-			collider := New(DefaultO)
-			a := collider.Insert(agent.O{
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
+			a := db.AgentInsert(agent.O{
 				Position:           vector.V{60.0040783686527, 80.40391843262739},
+				TargetVelocity:     vector.V{10, 10},
 				Velocity:           vector.V{10, 10},
 				Heading:            polar.V{1, 1.550798992821703},
 				Radius:             10,
 				MaxVelocity:        100,
 				MaxAngularVelocity: math.Pi / 2,
 				MaxAcceleration:    5,
-				Mask:               mask.MSizeSmall,
+				Flags:              flags.FSizeSmall,
 			})
-			collider.InsertFeature(feature.O{
+			db.FeatureInsert(feature.O{
 				Min: vector.V{70, 20},
 				Max: vector.V{90, 80},
 			})
@@ -379,6 +206,7 @@ func TestTick(t *testing.T) {
 			return config{
 				name:     "Experimental/CutCorner",
 				collider: collider,
+				db:       db,
 				d:        20 * time.Millisecond,
 				want: map[id.ID]vector.V{
 					a.ID(): vector.V{
@@ -394,7 +222,7 @@ func TestTick(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			c.collider.Tick(c.d)
 			for x, want := range c.want {
-				got := c.collider.GetOrDie(x).Position()
+				got := c.db.AgentGetOrDie(x).Position()
 				if !vector.Within(got, want) {
 					t.Errorf("Position() = %v, want = %v", got, want)
 				}
@@ -428,38 +256,40 @@ func BenchmarkTick(b *testing.B) {
 			min := 0.0
 			max := math.Sqrt(area)
 
-			collider := New(DefaultO)
+			db := database.New(database.DefaultO)
+			collider := New(O{DB: db, PoolSize: DefaultO.PoolSize})
 			for i := 0; i < c.n; i++ {
-				collider.Insert(agent.O{
+				db.AgentInsert(agent.O{
 					Radius:             R,
 					Position:           rv(min, max),
+					TargetVelocity:     rv(-1, 1),
 					Velocity:           rv(-1, 1),
 					MaxVelocity:        60,
 					MaxAcceleration:    10,
 					MaxAngularVelocity: math.Pi / 4,
 					Heading:            polar.V{1, 0},
-					Mask:               mask.MSizeSmall,
+					Flags:              flags.FSizeSmall,
 				})
 			}
 
 			// Add world borders.
 			// Add xmin border.
-			collider.InsertFeature(feature.O{
+			db.FeatureInsert(feature.O{
 				Min: vector.V{min - 1, min - 1},
 				Max: vector.V{min, max + 1},
 			})
 			// Add xmax border.
-			collider.InsertFeature(feature.O{
+			db.FeatureInsert(feature.O{
 				Min: vector.V{max, min - 1},
 				Max: vector.V{max + 1, max + 1},
 			})
 			// Add ymin border.
-			collider.InsertFeature(feature.O{
+			db.FeatureInsert(feature.O{
 				Min: vector.V{min, min - 1},
 				Max: vector.V{max, min},
 			})
 			// Add ymax border.
-			collider.InsertFeature(feature.O{
+			db.FeatureInsert(feature.O{
 				Min: vector.V{min, max},
 				Max: vector.V{max, max + 1},
 			})
